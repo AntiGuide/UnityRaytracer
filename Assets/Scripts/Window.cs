dd94563b9e7d1f69c9a7959097b9beb17a88bd94
Lukas Wiedemann <lukaswiedemann@gmail.com>
2019-07-09 10:58:29 +0200

    Created base render class

--- Assets/Scripts/Window.cs
+++ Assets/Scripts/Window.cs
@@ -1,11 +1,35 @@
-﻿using UnityEngine;
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UI;
 
-public class Window : MonoBehaviour {
-    void Start() {
-        
+public class Window : SerializedMonoBehaviour {
+
+    [SerializeField] private RawImage rawImage;
+
+    [SerializeField] private int resolutionWidth;
+    
+    [SerializeField] private int resolutionHeight;
+    
+    private Texture2D rawImageTexture;
+
+    private void Start() {
+        rawImageTexture = new Texture2D(resolutionWidth, resolutionHeight);
+        SetFillColor(Color.green);
     }
 
-    void Update() {
+    private void SetFillColor(Color color) {
+        var colors = new Color[rawImageTexture.width * rawImageTexture.height];
         
+        for (var index = 0; index < colors.Length; index++) {
+            colors[index] = color;
+        }
+
+        rawImageTexture.SetPixels(colors);
+        rawImageTexture.Apply();
+        rawImage.texture = rawImageTexture;
+    }
+
+    public void SetPixel(int x, int y, Color color) {
+        rawImageTexture.SetPixel(x, y, color);
     }
 }
